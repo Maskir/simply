0cb1863f9fb5b4298062ba43a4bf9edb31b68934
Maskir <1f479f0b@opayq.com>
2014-01-25 21:28:27 +0400

    3

--- simply.js
+++ simply.js
@@ -5,7 +5,7 @@ navigator.geolocation.getCurrentPosition(function(pos){
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.name, subtitle: data.main.temp });
+    simply.text({ title: data.name, subtitle: data.main.temp, subtitle: weatherUrl});
   });
 });
 
